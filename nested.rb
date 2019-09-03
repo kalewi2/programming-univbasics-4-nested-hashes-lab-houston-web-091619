87c5803f6e3665303ad6455e76e516e7bad3c15d
Kim Lewis <kalewi2@gmail.com>
2019-09-03 16:13:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -135,7 +135,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages] = ["Assembly", "C"]
+programmer_hash[:dennis_ritchie][:languages]push("Assembly")
 return programmer_hash
 
 end
